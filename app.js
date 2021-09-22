b590da0331b927fae6b278aee9367f0d3433b4df
Arpita Dasgupta <dasguptaarpita1999@gmail.com>
2021-09-22 23:10:09 +0530

    document.querySelector

---
+++ app.js
@@ -0,0 +1,12 @@
+const billAmt = document.querySelector("#billAmt");
+const cashGiven = document.querySelector("#cashGiven");
+
+const errorDiv = document.querySelector(".errorMsg");
+
+const cashGivenDiv = document.querySelector(".cashGivenInput");
+const changeReturnDiv = document.querySelector(".changeReturn");
+
+const output= document.querySelector("#output");
+
+const nextBtn = document.querySelector("#nextBtn");
+const checkBtn = document.querySelector("#checkBtn");
